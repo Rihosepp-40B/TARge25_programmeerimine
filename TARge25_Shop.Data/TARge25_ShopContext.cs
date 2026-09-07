5231402b8ed45eb1708e52ace390a9557c005c83
simoerberg <simoerberg19@gmail.com>
2026-09-07 15:35:59 +0300

    DBContextsetup

---
+++ TARge25_Shop.Data/TARge25_ShopContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TARge25_Shop.Data
+{
+    // Nimetasime classi TARge25_ShopContext, mis pärib DBContext klassi
+    public class TARge25_ShopContext : DbContext
+    {
+        // Tegime konteksti, mis pärib DBContext klassi
+        public TARge25_ShopContext(DbContextOptions<TARge25_ShopContext> option) : base(option) // consturctor
+        { }
+
+    }
+}
