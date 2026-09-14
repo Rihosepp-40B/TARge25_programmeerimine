97cd2e9919fd1fbac5148e1c9a62defba44f5eb6
Rihosepp-40B <rihosepp@gmail.com>
2026-09-14 17:42:45 +0300

    Spaceship update + start delete

--- TARge25Shop.ApplicationServices/Services/SpaceshipServices.cs
+++ TARge25Shop.ApplicationServices/Services/SpaceshipServices.cs
@@ -1,4 +1,5 @@
-﻿using TARge25_Shop.Core.Domain;
+﻿using Microsoft.EntityFrameworkCore;
+using TARge25_Shop.Core.Domain;
 using TARge25_Shop.Core.Dto;
 using TARge25_Shop.Core.ServiceInterface;
 using TARge25_Shop.Data;
@@ -24,7 +25,7 @@ namespace TARge25_Shop.ApplicationServices.Services
         {
             //siin peab tegema vaheinstansi dto ja domain vahel,
             //et andmed liiguvad dto-st domain objekt
-            Spaceship spaceShip = new();
+            Spaceship spaceShip = new Spaceship();
 
             spaceShip.Id = Guid.NewGuid();
             spaceShip.Name = dto.Name;
@@ -35,10 +36,47 @@ namespace TARge25_Shop.ApplicationServices.Services
             spaceShip.UpdatedAt = DateTime.Now;
 
             // Andmete salvestamine andmebaasi
-            _context.Spaceships.Add(spaceShip);
+            await _context.Spaceships.AddAsync(spaceShip);
             await _context.SaveChangesAsync();
 
             return spaceShip;
+        }
+
+        public async Task<Spaceship> Update(SpaceshipDto dto)
+        {
+            Spaceship spaceShip = new();
+
+            spaceShip.Id = dto.Id;
+            spaceShip.Name = dto.Name;
+            spaceShip.ShipType = dto.ShipType;
+            spaceShip.Crew = dto.Crew;
+            spaceShip.EnginePower = dto.EnginePower;
+            spaceShip.CreatedAt = dto.CreatedAt;
+            spaceShip.UpdatedAt = DateTime.Now;
+
+            _context.Spaceships.Update(spaceShip);
+            await _context.SaveChangesAsync();
+
+            return spaceShip;
+        }
+        //teha update meetod, mis võtab vastu dto ja uuendab olmasolevat kosmoselaeva
+        public async Task<Spaceship> DetailAsync(Guid id)
+        {
+            var spaceship = await _context.Spaceships
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            return spaceship;
+        }
+
+        public async Task<Spaceship> Delete(Guid id)
+        {
+            var result = await _context.Spaceships
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            _context.Spaceships.Remove(result);
+            await _context.SaveChangesAsync();
+
+            return result;
         }
     }
 }
--- TARge25_Shop/Controllers/SpaceshipController.cs
+++ TARge25_Shop/Controllers/SpaceshipController.cs
@@ -65,5 +65,56 @@ namespace TARge25_Shop.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(Guid id)
+        {
+            var spaceship = await _spaceshipServices.DetailAsync(id);
+
+            if (spaceship == null)
+            { return NotFound(); }
+
+            var vm = new SpaceshipUpdateViewModel
+            {
+                Id = spaceship.Id,
+                Name = spaceship.Name,
+                ShipType = spaceship.ShipType,
+                Crew = spaceship.Crew,
+                EnginePower = spaceship.EnginePower,
+                CreatedAt = spaceship.CreatedAt,
+                UpdatedAt = spaceship.UpdatedAt
+
+            };
+
+            return View(vm);
+
+        }
+
+        public async Task<IActionResult> Update(SpaceshipUpdateViewModel vm)
+        {
+            var dto = new SpaceshipDto
+            {
+                Id = vm.Id,
+                Name = vm.Name,
+                ShipType = vm.ShipType,
+                Crew= vm.Crew,
+                EnginePower= vm.EnginePower,
+                CreatedAt = vm.CreatedAt,
+                UpdatedAt = vm.UpdatedAt
+            };
+
+            var result = await _spaceshipServices.Update(dto);
+            if (result == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(Guid Id)
+        {
+            return View();
+        }
+        
     }
 }
