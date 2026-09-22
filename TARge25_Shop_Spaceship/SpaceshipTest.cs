80bb78a65d472748148e114c55bcc69359372452
Rihosepp-40B <rihosepp@gmail.com>
2026-09-22 19:05:24 +0300

    Two Spaceship tests

---
+++ TARge25_Shop_Spaceship/SpaceshipTest.cs
@@ -0,0 +1,59 @@
+﻿using TARge25_Shop.Core.Dto;
+using TARge25_Shop.Core.ServiceInterface;
+using Xunit;
+
+namespace TARge25_Shop.SpaceshipTest
+{
+    public class SpaceshipTest : TestBase
+    {
+        [Fact] // Fact tähistab ära ühte testi xUnit raamistikus
+        // 1 - kirjeldatakse ära kas test on tavaline või negatiivne
+        // 2 - Kirjeldatakse ära mida parasjagu üritatakse testialuse objektiga teha
+        // 3- Mis tingimustel tulemust kontrollitakse, peale tegevust
+        //
+        // Selles testis kontrollitakse et (2) Kosmoselaeva lisamisel (1) Ei tohiks (3) tulemus olla tühi. Jälgi seda sõnastusviisi:
+        //
+        //                  1           2               3
+        //                  \/          \/              \/
+        public async Task ShouldNot_AddEmptySpaceShip_WhenResultIsReturned()
+        {
+            // Ülesseade
+            SpaceshipDto dto = new SpaceshipDto()
+            {
+                Name = "X AE a L 10",
+                ShipType = "Taldrik",
+                Crew = 67,
+                EnginePower = 69,
+                UpdatedAt = DateTime.Now,
+                CreatedAt = DateTime.Now
+            };
+
+            //tegutsemine 
+            var result = await Svc<ISpaceshipServices>().Create(dto);
+
+            //Kontroll
+            Assert.NotNull(result);
+
+        }
+
+        [Fact]
+
+        // Selles testis kontrollitakse et (2) Kosmoselaeva päringul DB'st (1) Ei tohiks tagastada objekti (3) kui ID'd ei ole samad:
+        //
+        //                  1           2               3
+        //                  \/          \/              \/
+        public async Task ShouldNot_GetSpaceshipByID_WhenIDNotEqual()
+        {
+            // Ülesseade
+            Guid wrongGuid = Guid.NewGuid();
+            Guid goodGuid = Guid.Parse("9def918e-2eee-41b2-963e-31c34f4d7500");
+
+            // Tegvus
+            await Svc<ISpaceshipServices>().DetailAsync(goodGuid);
+
+            //Kontroll
+            Assert.NotEqual(wrongGuid, goodGuid);
+
+        }
+    }
+}
---
+++ TARge25_Shop_Spaceship/TestBase.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TARge25_Shop.ApplicationServices.Services;
+using TARge25_Shop.Core.ServiceInterface;
+using TARge25_Shop.Data;
+using TARge25_Shop.SpaceshipTest.Macros;
+using TARge25_Shop.SpaceshipTest.Mock;
+
+namespace TARge25_Shop.SpaceshipTest
+{
+    public abstract class TestBase
+    {
+        protected IServiceProvider serviceProvider {  get; set; }
+
+        protected TestBase()
+        {
+            var services = new ServiceCollection();
+            SetupServices(services);
+
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        // <summary>
+        // Seame üles testide läbiviimiseks vajalikud teenused mujalt projektidest
+        // See meetod annab ka mälusoleva andmebaasi mida testideks kasutada
+        // toimib kui "program.cs"-i sisu testide jooksutamiseks, ent lühidal kujul.
+        // </summary>
+        // <param name="services">tühi ServiceCollection-tüüpi muutuja kuhu astame
+        //teenused, sh ka andmebaasi. </param>
+
+        public virtual void SetupServices(ServiceCollection services)
+        {
+            services.AddScoped<ISpaceshipServices, SpaceshipServices>();
+            services.AddScoped<IFileServices, FileServices>();
+            services.AddScoped<IHostEnvironment, MockIHostEnvironment>();
+
+            services.AddDbContext<TARge25_ShopContext>(
+                x =>
+                {
+                x.UseInMemoryDatabase("Test");
+                x.ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+                }
+                );
+            RegisterMacros(services);
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        // <summary>
+        // Leia üles kindel teenus, teenusepakkujalt.
+        // ServiceProvider omab teenuseid, getService hangiv, X tüüpi teenuse,
+        // C# on ükskõik mis tüüpi võimalik ilma tüübita näidata tähe "T"-ga ehk "Template"
+        // </summary>
+        // <typeparam name="T"><typeparam>
+        // <returns></returns>
+
+        protected T Svc<T>()
+        {
+            return serviceProvider.GetService<T>();
+        }
+        // <summary>
+        // Registreerib macrodest teenuseid kui nad ei ole liidesed ja ei ole abstraktsed
+        // On vaja testi setupide seadistuseks
+        // <summary>
+        // <param name="services">Teenused, kuhu lusab makrodest muid teenuseid</param>
+
+        private void RegisterMacros(ServiceCollection services)
+        {
+            var macroBaseType = typeof(IMacros);
+
+            var macros = macroBaseType.Assembly.GetTypes()
+                .Where(t => macroBaseType.IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract);
+
+            foreach (var macro in macros) 
+            {
+                services.AddSingleton(macros);
+            }
+        }
+    }
+}
